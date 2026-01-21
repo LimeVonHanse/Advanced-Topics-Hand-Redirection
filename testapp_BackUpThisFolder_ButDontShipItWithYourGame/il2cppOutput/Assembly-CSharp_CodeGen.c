9ee2dfcc8ae86a2d944f30094ffb472af5bcbdd3
LimeVonHanse <emil.hansen98@gmail.com>
2026-01-21 17:11:03 +0100

    Initial commit

---
+++ testapp_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,57 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void SceneManager_Start_mA38706E4402D600ECDE8A1FF3C45DDEF3948DB0F (void);
+extern void SceneManager_Update_mF9680B5FCF65C2E1ABFB3729BEA27C1A49B4D2B0 (void);
+extern void SceneManager__ctor_m61FBFE598A99CDD806C72972FD6466E89648A5B1 (void);
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	SceneManager_Start_mA38706E4402D600ECDE8A1FF3C45DDEF3948DB0F,
+	SceneManager_Update_mF9680B5FCF65C2E1ABFB3729BEA27C1A49B4D2B0,
+	SceneManager__ctor_m61FBFE598A99CDD806C72972FD6466E89648A5B1,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	20489,
+	20489,
+	20489,
+	20489,
+	20489,
+	33592,
+	20489,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
